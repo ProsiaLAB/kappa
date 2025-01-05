02792627687dad0381bc542a17740467f1605f2c
kmaitreys <km.maitrey@gmail.com>
2025-01-05 10:47:17 +0530

    ex: add examples

---
+++ examples/run.jl
@@ -0,0 +1,8 @@
+using Opacities
+
+# println("Calling a function from Fractal:")
+# Opacities.Fractal.fractal_function("test")
+
+# println("Calling a function from Mie:")
+Opacities.Mie.add_two(4)
+
