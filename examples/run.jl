16cb52b6981400fe835fa632116c23699aa13adb
kmaitreys <km.maitrey@gmail.com>
2025-02-11 00:33:20 +0530

    remove junk

--- examples/run.jl
+++
@@ -1,8 +0,0 @@
-using Opacities
-
-# println("Calling a function from Fractal:")
-# Opacities.Fractal.fractal_function("test")
-
-# println("Calling a function from Mie:")
-Opacities.Mie.add_two(4)
-
