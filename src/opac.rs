cce6e1d5a3dae09f5ce1240230d9e619a522a225
kmaitreys <km.maitrey@gmail.com>
2025-03-11 00:26:18 +0530

    feat: implement Bruggeman and Maxwell-Garnet blending functions

--- src/opac.rs
+++ src/opac.rs
@@ -136,4 +136,45 @@ pub enum KappaError {
     InvalidFeature,
     InvalidPorosity,
     IncompatibleArguments(String),
+fn bruggeman_blend(abun: &[f64], e_in: &[Complex64]) -> Result<Complex64, KappaError> {
+    let mut abunvac = 1.0 - abun.iter().sum::<f64>();
+    let mvac = Complex::new(1.0, 0.0);
+    if abunvac < 0.0 {
+        if abunvac.abs() < 1e-5 {
+            abunvac = 0.0;
+        } else {
+            return Err(KappaError::UnnormalizedAbundance);
+        }
+    }
+
+    // Blend iteratively
+    let mut mm = mvac;
+    let mut tot: Complex64 = Complex::new(0.0, 0.0);
+    let mut me: Complex64 = Complex::new(0.0, 0.0);
+    for _ in 0..100 {
+        tot = e_in
+            .iter()
+            .zip(abun.iter())
+            .map(|(&e_in_j, &abun_j)| {
+                (e_in_j.powi(2) - mm.powi(2)) / (e_in_j.powi(2) + 2.0 * mm.powi(2)) * abun_j
+            })
+            .sum::<Complex64>()
+            + ((mvac.powi(2) - mm.powi(2)) / (mvac.powi(2) + 2.0 * mm.powi(2)) * abunvac);
+
+        me = mm * ((2.0 * tot + 1.0) / (1.0 - tot)).sqrt();
+        mm = me;
+    }
+    if tot.abs() / mm.abs() > 1e-6 {
+        eprintln!("WARNING: Bruggeman blend not converged");
+    }
+    Ok(me)
+}
+
+fn maxwell_garnet_blend(m1: Complex64, m2: Complex64, vf_m: f64) -> (f64, f64) {
+    let vf_c = 1.0 - vf_m;
+    let me = m2.powi(2)
+        * ((2.0 * m2.powi(2) + m1.powi(2) - 2.0 * vf_c * (m2.powi(2) - m1.powi(2)))
+            / (2.0 * m2.powi(2) + m1.powi(2) + vf_c * (m2.powi(2) - m1.powi(2))));
+
+    (me.sqrt().re, me.sqrt().im)
 }
