e075438a02b6ff19b85a53b34f3b2321b4e3174b
kmaitreys <km.maitrey@gmail.com>
2025-03-15 00:35:39 +0530

    refac: my new glorious cli :)

--- src/cli.rs
+++ src/cli.rs
@@ -10,20 +10,30 @@ use crate::opac::KappaError;
 pub fn launch() -> Result<(), KappaError> {
     let mut args = env::args().skip(1).peekable();
     if args.peek().is_none() {
-        print_usage();
         return Ok(());
     }
 
     while let Some(arg) = args.next() {
         match arg.as_str() {
-            "-h" | "--help" => {
-                print_usage();
+            "-L" | "--list" => {
+                list_materials();
                 return Ok(());
             }
-            "-q" => {
+            "-c" => {
+                process_materials();
+            }
+            "-h" => {
+                print_short_help();
+                return Ok(());
+            }
+            "--help" => {
+                print_long_help();
+                return Ok(());
+            }
+            "-q" | "--quiet" => {
                 todo!()
             }
-            "-v" => {
+            "-v" | "--version" => {
                 todo!()
             }
             _ => {
@@ -37,35 +47,159 @@ pub fn launch() -> Result<(), KappaError> {
     Ok(())
 }
 
-fn print_usage() {
+fn process_materials() {
+    println!("Processing materials...");
+}
+
+fn print_short_help() {
+    // Description
     print!("{}", "kappa".bold().color(Color::Yellow));
     print!(" : ");
     println!("Dust opacities from the command line");
-    println!("Dominik, Min, Tazaki 2021, https://ascl.net/2104.010, version 1.9.14");
+    println!("        Dominik, Min, Tazaki 2021, https://ascl.net/2104.010, version 1.9.14");
     println!();
+
+    // Usage
     print!("{}", "Usage".bold().underline().color(Color::Green));
     println!(":");
-    println!("kappa [OPTIONS] [MATERIAL [MFRAC]]...");
+    println!("  kappa [OPTIONS] [MATERIAL [MFRAC]]...");
     println!();
+
+    // Arguments
     print!("{}", "Arguments".bold().underline().color(Color::Cyan));
     println!(":");
-    print!("{:<25}", "MATERIAL [MFRAC]...");
+    print!("{:<25}", "  <MATERIAL> [MFRAC]...");
     println!("Specify materials. Example: c-gra 0.5 c-sil 0.3");
     println!();
+
+    // Options
     print!("{}", "Options".bold().underline().color(Color::Blue));
     println!(":");
-    print!("{:<40}", "-L, --list".bold());
-    println!("List built-in materials");
-    print!("{:<2}{:<33}", "-c".bold(), " MATERIAL [MFRAC]...");
-    println!("Specify a material to include in the grain, with its mass fraction (default: 1.0)");
-    print!("{:<2}{:<33}", "-m".bold(), " MATERIAL [MFRAC]...");
-    println!("Specify a material to include in the mantle, with its mass fraction (default: 0.0)");
-    print!(
-        "{:<2}{:<33}",
-        "-p".bold(),
-        " CORE_POROSITY [MANTLE_POROSITY]"
+
+    // General options (grain composition)
+    print!("{}", "GRAIN COMPOSITION".color(Color::BrightMagenta));
+    println!(":");
+    println!("{}", "  -L, --list".bold());
+    println!("      List built-in materials");
+    println!("{}", "  -c".bold().to_string() + " <MATERIAL> [MFRAC]...");
+    println!(
+        "      Specify a material to include in the grain, with its mass fraction (default: 1.0)"
     );
-    println!("Specify core and mantle porosities (default: 0.0)");
+    println!("{}", "  -m".bold().to_string() + " <MATERIAL> [MFRAC]...");
+    println!(
+        "      Specify a material to include in the mantle, with its mass fraction (default: 0.0)"
+    );
+    println!(
+        "{}",
+        "  -p".bold().to_string() + " <CORE_POROSITY> [MANTLE_POROSITY]..."
+    );
+    println!("      Specify core and mantle porosities (default: 0.0)");
+    println!("{}", "  --diana".bold());
+    println!("      Use the DIANA composition (Woitke et al. 2016)");
+    println!("{}", "  --dsharp".bold());
+    println!("      Use the DSHARP composition (Birnstiel et al. 2018)");
+    println!("{}", "  --dsharp-no-ice".bold());
+    println!("      Use the DSHARP composition without ice");
+    println!();
+
+    // Grain geometry and computational method options
+    print!(
+        "{}",
+        "GRAIN GEOMETRY AND COMPUTATIONAL METHOD".color(Color::BrightMagenta)
+    );
+    println!(":");
+    println!("{}", "  --dhs".bold().to_string() + " [FMAX]");
+    println!("      Use the Distribution of Hollow Spheres (DHS) approach to model deviations");
+    println!(
+        "{}",
+        "  --mmf".bold().to_string() + " [A0 [DFRAC-OR-FILL [KF]]]"
+    );
+    println!("      Use the Modified Mean Field (MMF) theory from Tazaki & Tanaka (2017)");
+    println!("{}", "  --mie".bold());
+    println!("      Do a standard Mie calculation for perfect spheres");
+    println!("      Equivalent to --dhs 0.0");
+    println!("{}", "  --cde".bold());
+    println!(
+        "      Compute Continuous Distribution of Ellipsoids (CDE) opacities in the Rayleigh limit"
+    );
+    println!();
+
+    // Grain size distribution options
+    print!("{}", "GRAIN SIZE DISTRIBUTION".color(Color::BrightMagenta));
+    println!(":");
+    println!(
+        "{}",
+        "  -a".bold().to_string() + " <AMIN> [AMAX [APOW [NA]]]"
+    );
+    println!("      Specify a power-law grain size distribution");
+    println!(
+        "{}",
+        "  -a".bold().to_string() + " <AMIN AMAX AMEAN:ASIG> [NA]"
+    );
+    println!("      Specify a [log-]normal grain size distribution");
+    println!("{}", "  -a".bold().to_string() + " <FILE>");
+    println!("      Read grain size distribution from a file");
+    println!("{}", "  --amin".bold().to_string() + " <AMIN>");
+    println!("      Override minimum grain size");
+    println!("{}", "  --amax".bold().to_string() + " <AMAX>");
+    println!("      Override maximum grain size");
+    println!("{}", "  --amean".bold().to_string() + " <AMEAN>");
+    println!("      Override centroid size");
+    println!("{}", "  --asig".bold().to_string() + " <ASIG>");
+    println!("      Override logarithmic width");
+    println!("{}", "  --apow".bold().to_string() + " <APOW>");
+    println!("      Override power-law index");
+    println!("{}", "  --na".bold().to_string() + " <NA>");
+    println!("      Override number of size bins");
+    println!();
+
+    // Wavelength options
+    print!("{}", "WAVELENGTH GRID".color(Color::BrightMagenta));
+    println!(":");
+    println!("{}", "  -l".bold().to_string() + " <LMIN> [LMAX [NLAM]]");
+    println!("      Specify a wavelength grid");
+    println!("{}", "  -l".bold().to_string() + " <FILE>");
+    println!("      Read wavelength grid from a file");
+    println!("{}", "  --lmin".bold().to_string() + " <LMIN>");
+    println!("      Override minimum wavelength");
+    println!("{}", "  --lmax".bold().to_string() + " <LMAX>");
+    println!("      Override maximum wavelength");
+    println!("{}", "  --nlam".bold().to_string() + " <NLAM>");
+    println!("      Override number of wavelength points");
+    println!();
+
+    // Output options
+    print!("{}", "OUTPUT".color(Color::BrightMagenta));
+    println!(":");
+    println!("{}", "  -o".bold().to_string() + " [DIR]");
+    println!("      Write output to a directory (default: current directory)");
+    println!("{}", "  -s".bold().to_string() + " [NANG]");
+    println!("      Include scattering matrix in the output");
+    println!("{}", "  -d".bold().to_string() + " [NSUB]");
+    println!("      Divide computation upto into n_a parts");
+    println!("{}", "  --chop".bold().to_string() + " [NDEG]");
+    println!("      Cap the first NDEG degrees of the forward scattering peak");
+    println!("{}", "  --fits".bold());
+    println!("      Write output in FITS format");
+    println!("{}", "  --radmc".bold().to_string() + " [LABEL]");
+    println!("      Write output in RADMC-3D format");
+    println!("{}", "  -w".bold());
+    println!("      Create files on which kappa can operate");
+    println!();
+
+    // Miscellaneous options
+    print!("{}", "MISCELLANEOUS".color(Color::BrightMagenta));
+    println!(":");
+    println!("{}", "  -h, --help".bold());
+    println!("      Print this help message");
+    println!("{}", "  -q, --quiet".bold());
+    println!("      Quiet mode");
+    println!("{}", "  -v, --version".bold());
+    println!("      kappa version");
+}
+
+fn print_long_help() {
+    todo!()
 }
 
 pub fn list_materials() {
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use kappa::{cli, opac::KappaError};
 
 fn main() -> Result<(), KappaError> {
     cli::launch()?;
+    println!("No arguments provided. Running in default mode.");
 
     Ok(())
 }
