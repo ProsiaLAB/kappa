f50cea5e68fe51ddcab789d19ebe445c8fa7cf33
kmaitreys <km.maitrey@gmail.com>
2025-03-27 19:57:47 +0530

    feat: new type alias for Array view

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,9 @@
+use ndarray::ArrayView1;
 use ndarray::{Array1, Array2, Array3};
 use num_complex::Complex;
 
 pub type RVector = Array1<f64>;
+pub type RVecView<'a> = ArrayView1<'a, f64>;
 pub type CVector = Array1<Complex<f64>>;
 pub type RMatrix = Array2<f64>;
 pub type CMatrix = Array2<Complex<f64>>;
